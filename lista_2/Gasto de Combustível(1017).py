66c9b3933cccd928e884f630ef5e44eabf4f5f00
caua2512 <130388387+caua2512@users.noreply.github.com>
2023-04-23 14:13:03 -0300

    Delete Gasto de Combustível(1017).py

--- lista_2/Gasto de Combustível(1017).py
+++
@@ -1,7 +0,0 @@
-tempo_de_viagem = int(input())
-velocidade_media = int(input())
-
-distancia_percorrida = velocidade_media * tempo_de_viagem
-gasto = distancia_percorrida / 12
-
-print(f'{gasto:.3f}')
