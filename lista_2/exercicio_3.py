b1f5e157767346fa427ee1756c460a079e7703d4
caua2512 <130388387+caua2512@users.noreply.github.com>
2023-04-23 20:49:36 -0300

    Create exercicio_3.py

---
+++ lista_2/exercicio_3.py
@@ -0,0 +1,8 @@
+print('Digite a nota do primeiro bimestre da disciplina:')
+nota_1 = int(input())
+print('Digite a nota do segundo bimestre da disciplina:')
+nota_2 = int(input())
+
+calculo = (nota_1 * 2 + nota_2 * 3) // 5
+
+print(f'Média parcial = {calculo}')
